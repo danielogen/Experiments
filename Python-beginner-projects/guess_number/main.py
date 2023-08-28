0dbf72d4ccf222646fc732c15d6a9956383c7d04
danielogen <danielogen@gmail.com>
2023-08-28 11:54:15 -0700

    ft: computer guess number

--- Python-beginner-projects/guess_number/main.py
+++ Python-beginner-projects/guess_number/main.py
@@ -12,4 +12,29 @@ def guess(x):
             print('Oops!!, your guess is too low')
     print(f"Wow!! congrats!, you have guessed the random number {random_number}")
 
-guess(10)
+# guess(10)
+
+"""The computer will try to guess our secret number"""
+
+def computer_guess(x, number):
+    print(f"Your secret number is {number}. I will now try to guess it")
+    low = 1
+    high = x
+
+    feedback = ''
+
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low # could also be high b/c low == high
+
+        feedback = input(f"Is {guess} too low [H] or too high [H], Or correct [C]: ")
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print(f"Yay! I guessed your secret number correctly")
+            
+            
+computer_guess(1000, 300)
